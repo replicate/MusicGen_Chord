chord_chroma masking fixed

audiocraft/models/musicgen.py
@@ -328,8 +328,8 @@ class MusicGen:
                     )
                 else :
                     attr.wav['self_wav'] = WavChordTextCondition(
-                        melody,
-                        torch.tensor([0], device=self.device),
+                        [melody],
+                        torch.tensor([self.duration*self.sample_rate], device=self.device),
                         sample_rate=[self.sample_rate],
                         path=[None],
                         bpm = [bpm],

audiocraft/modules/conditioners.py
@@ -171,13 +171,13 @@ def nullify_wav(cond: tp.Union[WavCondition,WavChordTextCondition]) -> tp.Union[
         )
     else:
         return WavChordTextCondition(
-            wav='N',
-            length=cond.length,
+            wav=['N']* len(cond.wav),
+            length=torch.tensor([0] * len(cond.wav), device=cond.length.device),
             sample_rate=cond.sample_rate,
             path=[None],
             seek_time=[None],
             bpm = cond.bpm,
-            meter = cond.meter,
+            meter = cond.meter
         )
 
 
@@ -902,6 +902,7 @@ class ChromaChordConditioner(ChromaStemConditioner):
                 chroma = torch.zeros([self.chroma_len, self.dim])
                 count = 0
                 offset = 0
+                
                 stext = wav.split(" ")
                 barsec = 60/(bpm/meter)
                 timebin = barsec * self.bar2chromabin
@@ -1534,7 +1535,7 @@ class ConditioningProvider(nn.Module):
                     wavs[attribute].append(wav.flatten())  # [T]
                 else:
                     wav, length, sample_rate, path, seek_time, bpm, meter = sample.wav[attribute]
-                    wavs[attribute].append(wav)
+                    wavs[attribute].append(wav[0])
                     bpms[attribute].append(bpm[0])
                     meters[attribute].append(meter[0])
                 lengths[attribute].append(length)

inference_chord.py
@@ -2,6 +2,7 @@ import torchaudio
 from audiocraft.models import MusicGen
 from audiocraft.data.audio import audio_write
 from audiocraft.modules.conditioners import ChromaChordConditioner
+from audiocraft.solvers.compression import CompressionSolver
 import pandas as pd
 from pathlib import Path
 from tqdm import tqdm
@@ -9,17 +10,51 @@ import random
 import torch.nn as nn
 import os 
 import math
+import torch
+from omegaconf import OmegaConf
+from audiocraft.models.builders import get_lm_model
 
 # os.environ["CUDA_VISIBLE_DEVICES"] = '1'
 
-model = MusicGen.get_pretrained('facebook/musicgen-melody', device='cuda')
+device = 'cuda'
+
+loaded = torch.load("/home/sake/musicgen_chord.th")
+def _delete_param(cfg, full_name: str):
+    parts = full_name.split('.')
+    for part in parts[:-1]:
+        if part in cfg:
+            cfg = cfg[part]
+        else:
+            return
+    OmegaConf.set_struct(cfg, False)
+    if parts[-1] in cfg:
+        del cfg[parts[-1]]
+    OmegaConf.set_struct(cfg, True)
+
+# LM
+
+cfg = OmegaConf.create(loaded['xp.cfg'])
+cfg.device = str(device)
+if cfg.device == 'cpu':
+    cfg.dtype = 'float32'
+else:
+    cfg.dtype = 'float16'
+_delete_param(cfg, 'conditioners.self_wav.chroma_chord.cache_path')
+_delete_param(cfg, 'conditioners.self_wav.chroma_stem.cache_path')
+_delete_param(cfg, 'conditioners.args.merge_text_conditions_p')
+_delete_param(cfg, 'conditioners.args.drop_desc_p')
+
+lm = get_lm_model(loaded['xp.cfg'])
+lm.load_state_dict(loaded['best_state']['model']) 
+lm.eval()
+lm.cfg = cfg
+
+compression_model = CompressionSolver.wrapped_model_from_checkpoint(cfg, cfg.compression_model_checkpoint, device=device)
+
+model = MusicGen("sakemin/musicgen-chord", compression_model, lm)
+
 model.set_generation_params(duration=30)
-chordprovider = model.lm.condition_provider.conditioners.self_wav
-model.lm.condition_provider.conditioners.self_wav = ChromaChordConditioner(chordprovider.output_dim, chordprovider.sample_rate, chordprovider.chroma.n_chroma, int(math.log2(chordprovider.chroma.winlen)), chordprovider.duration, device='cuda')
-# model.to('cuda')
-# model.device='cuda:1'
-model.lm.to('cuda')
-# wav = model.generate_unconditional(4)    # generates 4 unconditional audio samples
+model.lm = model.lm.to(device)
 
 target_path = Path("/home/sake/chords_text_gen")
 
@@ -27,9 +62,9 @@ target_path = Path("/home/sake/chords_text_gen")
 
 # rand_idxs = random.sample(range(1, len(df)), 4)
 
-descriptions = ["happy piano pop song"]
-chord_text = 'C G Amin F'
-bpm = 137
+descriptions = ["bossa nova", "bossa nova"]
+chord_text = ['C G A:min F', 'F G E:min A:min']
+bpm = 60
 in_triple = False
 
 for i in range(len(descriptions)):
@@ -61,8 +96,8 @@ for i in tqdm(range(1)):
     # print(descriptions)
     # wav = model.generate(descriptions)  # generates 3 samples.
 
-    melody, sr = torchaudio.load('/home/sake/psycho_separated_drumless/mdx_extra/mdx_extra/psycho/no_drums.wav')
-    wav = model.generate_with_chroma(descriptions, melody[None], sr)
+    # melody, sr = torchaudio.load('/home/sake/01 Psycho.mp3')
+    # wav = model.generate_with_chroma(descriptions, melody[None], sr)
 
     wav = model.generate_with_text_chroma(descriptions, chord_text, bpm = bpm, in_triple = in_triple)
 
@@ -73,4 +108,4 @@ for i in tqdm(range(1)):
         # print(iidx, sidx)
         # path = target_path/paths[iidx]
         path.mkdir(parents=True, exist_ok=True)
-        audio_write(f'{str(path/str(i))}_{i}', one_wav.cpu(), model.sample_rate, strategy="loudness", loudness_compressor=True)
+        audio_write(f'{str(path/str(idx))}_{idx}', one_wav.cpu(), model.sample_rate, strategy="loudness", loudness_compressor=True)