masking for short audio chord condition fixed

audiocraft/modules/chord_chroma.py
@@ -21,13 +21,13 @@ class ChordExtractor(nn.Module):
 
     def __init__(self, device, sample_rate, max_duration, chroma_len, n_chroma, winhop):
         super().__init__()
-        self.config = HParams.load("/home/sake/musicgen/MusicGen_Chord/audiocraft/modules/btc/run_config.yaml") #gotta specify the path for run_config.yaml of btc
+        self.config = HParams.load("audiocraft/modules/btc/run_config.yaml") #gotta specify the path for run_config.yaml of btc
 
         # self.config.feature['large_voca'] = False
         # self.config.model['num_chords'] = 25
 
-        self.model_file = '/home/sake/musicgen/MusicGen_Chord/audiocraft/modules/btc/test/btc_model_large_voca.pt'
-        # self.model_file = '/home/sake/musicgen/MusicGen_Chord/audiocraft/modules/btc/test/btc_model.pt'
+        self.model_file = 'audiocraft/modules/btc/test/btc_model_large_voca.pt'
+        # self.model_file = 'audiocraft/modules/btc/test/btc_model.pt'
         self.idx_to_chord = idx2voca_chord()
         self.sr = sample_rate
 
@@ -123,7 +123,7 @@ class ChordExtractor(nn.Module):
 
             strlines = ''.join(lines)
 
-            chroma = torch.zeros([self.chroma_len,self.n_chroma])
+            chroma = []
 
             count = 0
             for line in lines:
@@ -146,9 +146,9 @@ class ChordExtractor(nn.Module):
                 for j in range(start_bin,end_bin):
                     if count >= self.chroma_len: 
                         break
-                    chroma[j]=multihot
+                    chroma.append(multihot)
                     count += 1
             
-            chromas.append(chroma)
+            chromas.append(torch.stack(chroma, dim=0))
         
         return torch.stack(chromas, dim=0).to(self.device)

audiocraft/modules/conditioners.py
@@ -937,7 +937,6 @@ class ChromaChordConditioner(ChromaStemConditioner):
                 chroma = chroma.repeat(1, n_repeat, 1)
                 chroma = chroma[:, :self.chroma_len]
                 logger.debug(f"Chroma was repeated to match length! ({T} -> {chroma.shape[1]})")
-
         return chroma
 
     def tokenize(self, x: tp.Union[WavCondition, WavChordTextCondition]) -> tp.Union[WavCondition, WavChordTextCondition]:
@@ -958,17 +957,25 @@ class ChromaChordConditioner(ChromaStemConditioner):
         """
         wav, lengths, *_ = x
         with torch.no_grad():
-            embeds = self._get_wav_embedding(x)
-        # print("CHROMA :::", embeds)
+            embeds = self._get_wav_embedding(x) #chroma
         embeds = embeds.to(self.output_proj.weight)
-        # embeds = embeds.to(self.output_proj[0].weight) #For MLP projection nn.Sequential
         embeds = self.output_proj(embeds)
 
-        if lengths is not None:
-            lengths = lengths / self._downsampling_factor()
-            mask = length_to_mask(lengths, max_len=embeds.shape[1]).int()  # type: ignore
+        if self.match_len_on_eval:
+            if lengths is not None:
+                for i in range(len(lengths)):
+                    if lengths[i] > 0 and lengths[i] < self.duration * self.sample_rate:
+                        lengths[i] = torch.Tensor([(self.duration+1) * self.sample_rate])
+                lengths = lengths / self._downsampling_factor()
+                mask = length_to_mask(lengths, max_len=embeds.shape[1]).int()  # type: ignore
+            else:
+                mask = torch.ones_like(embeds)
         else:
-            mask = torch.ones_like(embeds)
+            if lengths is not None:
+                lengths = lengths / self._downsampling_factor()
+                mask = length_to_mask(lengths, max_len=embeds.shape[1]).int()  # type: ignore
+            else:
+                mask = torch.ones_like(embeds)
 
         embeds = (embeds.to(self.device) * mask.unsqueeze(2).to(self.device))
 

inference_chord.py
@@ -18,7 +18,8 @@ from audiocraft.models.builders import get_lm_model
 
 device = 'cuda'
 
-loaded = torch.load("/home/sake/musicgen_chord.th")
+# loaded = torch.load("/home/sake/musicgen_chord.th")
+loaded = torch.load("/home/sake/500000.th")
 def _delete_param(cfg, full_name: str):
     parts = full_name.split('.')
     for part in parts[:-1]: